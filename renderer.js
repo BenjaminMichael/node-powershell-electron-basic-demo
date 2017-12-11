1ed8cc92f4c4818380f46332d3fe16b2611dd06c
ben <ben@m-c02sy0p7h03m.local>
2017-12-10 20:38:29 -0500

    test1

--- renderer.js
+++ renderer.js
@@ -1,3 +1,71 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+window.$ = window.jQuery = require('./node_modules/jquery/dist/jquery.js') // I use this when the NPM 'node_modules' folder is in my project and NOT installed globally.
+window.Hammer = require('./node_modules/materialize-css/js/hammer.min.js') // For the purpoises of this trick the sequence of these 3 requires is important
+require('materialize-css')
+
+const shell = require('node-powershell');
+var test1=[1,2,3,4,5,6,7,8,9]
+    test2=[1,2,3,4,5,6,7,8,9]
+    test3=[1,2,3,4,5,6,7,8,9]
+
+$('#test1Btn').click(()=>{
+  test1.map((value)=>{
+    let ps = new shell({
+      executionPolicy: 'Bypass',
+      noProfile: true
+    });
+    
+    ps.addCommand(`$x=$PSVersionTable;write-host 'zug-zug-${value}'`)
+    ps.invoke()
+    .then(output => {
+      $('#resultsGoHere').append(output)
+    })
+    .catch(err => {
+      console.log(err);
+      ps.dispose();
+    });
+  })
+})
+
+$('#test2Btn').click(()=>{
+    test2.map((value)=>{
+    setImmediate(
+        (()=>{
+        let ps = new shell({
+            executionPolicy: 'Bypass',
+            noProfile: true
+        });
+        
+        ps.addCommand(`$x=$PSVersionTable;write-host 'Work Complete: ${value}'`)
+        ps.invoke()
+        .then(output => {
+            $('#resultsGoHere').append(output)
+        })
+        .catch(err => {
+            console.log(err);
+            ps.dispose();
+        });
+        })
+    )
+    })
+})
+
+$('#test3Btn').click(()=>{
+    test3.map((value)=>{
+        process.nextTick(() => {
+            let ps = new shell({
+            executionPolicy: 'Bypass',
+            noProfile: true
+            });
+            
+            ps.addCommand(`$x=$PSVersionTable;write-host 'Lok-Tar! ${value}'`)
+            ps.invoke()
+            .then(output => {
+                $('#resultsGoHere').append(output)
+            })
+            .catch(err => {
+            console.log(err);
+            ps.dispose();
+            });
+        });
+    })
+})
