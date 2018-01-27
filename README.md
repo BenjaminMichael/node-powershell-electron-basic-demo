47e5e8f89e1f6f42a7d6ffda45c986678d4a3dac
ben <ben@m-c02sy0p7h03m.local>
2018-01-27 11:22:17 -0500

    final draft

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # benchmarking node-powershell command loops with electron-quick-start
-under construction
+
+If you install your npm modules globally you will probably want to fix 2 things:
+
+1 in index.html
+```html
+<link href="./node_modules/materialize-css/dist/css/materialize.css" rel="stylesheet">
+```
+
+and 1 in renderer.js
+``` javascript
+window.Hammer = require('./node_modules/materialize-css/js/hammer.min.js')
+```
--- renderer.js
+++ renderer.js
@@ -1,49 +1,39 @@
-window.$ = window.jQuery = require('./node_modules/jquery/dist/jquery.js') // I use this when the NPM 'node_modules' folder is in my project and NOT installed globally.
-window.Hammer = require('./node_modules/materialize-css/js/hammer.min.js') // For the purpoises of this trick the sequence of these 3 requires is important
+window.Hammer = require('./node_modules/materialize-css/js/hammer.min.js') 
 require('materialize-css')
+const shell = require('node-powershell')
 
-const shell = require('node-powershell');
+var testData=[1,2,3,4,5,6,7,8,9]
 
-
-
-
-var test1=[1,2,3,4,5,6,7,8,9]
-    test2=[1,2,3,4,5,6,7,8,9]
-    test3=[1,2,3,4,5,6,7,8,9]
-    test4=[1,2,3,4,5,6,7,8,9]
-    test5=[1,2,3,4,5,6,7,8,9]
-
-
-    function loading(){
-        const myPreLoader = `
-        <div class="preloader-wrapper big active">
-        <div class="spinner-layer spinner-blue">
-        <div class="circle-clipper left">
-            <div class="circle"></div>
-        </div><div class="gap-patch">
-            <div class="circle"></div>
-        </div><div class="circle-clipper right">
-            <div class="circle"></div>
-        </div>
-        </div>
-    `
-        $('#myH3').html(myPreLoader)
+function loading(){
+    const myPreLoader = `
+    <div class="preloader-wrapper big active">
+    <div class="spinner-layer spinner-blue">
+    <div class="circle-clipper left">
+        <div class="circle"></div>
+    </div><div class="gap-patch">
+        <div class="circle"></div>
+    </div><div class="circle-clipper right">
+        <div class="circle"></div>
+    </div>
+    </div>`
+    $('#myH3').html(myPreLoader)
+}
+function doneLoading(myResultant){
+    $('#myH3').html('<h2>Done.</h2>')
+    const allDone = () => {
+        $('#myH3').empty()
     }
-    function doneLoading(myResultant){
-        $('#myH3').html('<h2>Done.</h2>')
-        if(myResultant !== ""){
-            $('#resultsGoHere').append(`<div class="chip orange lighten-3 purple-text z-depth-3 text-darken-1 col s2 m2 l2">${myResultant}</div>`)
-        }
+    if(myResultant !== ""){
+        $('#resultsGoHere').append(`<div class="chip orange lighten-3 purple-text z-depth-3 text-darken-1 col s2 m2 l2">${myResultant}</div>`)
     }
-
-    function errorUIFeedback(){
-        $('#myH3').html()
-        $('#resultsGoHere').append(`<div class="chip red white-text col s2 m2 l2 flow-text">Error</div>`)
-    }
-
+    setTimeout(allDone(), 100)
+}
+function errorUIFeedback(){
+    $('#myH3').html()
+    $('#resultsGoHere').append(`<div class="chip red white-text col s2 m2 l2 flow-text">Error</div>`)
+}
 
 $(document).ready(function(){
-
     $('#cancelBtn').click( () => {
     $('#resultsGoHere').empty()
     })
@@ -54,7 +44,7 @@ $(document).ready(function(){
             executionPolicy: 'Bypass',
             noProfile: true
             });
-    test1.map((value)=>{
+    testData.map((value)=>{
         ps.streams.stdin.write(`$x=$PSVersionTable;write-host 'ZugZug: ${value}'
         `);
         })
@@ -62,12 +52,13 @@ $(document).ready(function(){
         ps.on('output', output=>{
             var parsedOutput = output.split("\n") 
             parsedOutput.forEach(val => doneLoading(val))
+            ps.dispose()
         }); 
     })
 
     $('#test2Btn').click(()=>{
        loading()
-        test2.map((value)=>{
+        testData.map((value)=>{
         setImmediate(
             (()=>{
             let ps = new shell({
@@ -81,9 +72,9 @@ $(document).ready(function(){
                 doneLoading(output)
             })
             .catch(err => {
-                errorUIFeedback();
-                console.log(err);
-                ps.dispose();
+                errorUIFeedback()
+                console.log(err)
+                ps.dispose()
             });
             })
         )
@@ -92,7 +83,7 @@ $(document).ready(function(){
 
     $('#test3Btn').click(()=>{
         loading()
-        test3.map((value)=>{
+        testData.map((value)=>{
             process.nextTick(() => {
                 let ps = new shell({
                 executionPolicy: 'Bypass',
@@ -102,12 +93,13 @@ $(document).ready(function(){
                 ps.addCommand(`$x=$PSVersionTable;write-host 'Lok-Tar! ${value}'`)
                 ps.invoke()
                 .then(output => {
+                    ps.dispose();
                     doneLoading(output)
                 })
                 .catch(err => {
-                    errorUIFeedback();
-                    console.log(err);
-                    ps.dispose();
+                    errorUIFeedback()
+                    console.log(err)
+                    ps.dispose()
                 });
             });
         })
@@ -115,7 +107,7 @@ $(document).ready(function(){
 
     $('#test4Btn').click( () => {
         loading()
-    test5.forEach((value, index)=>{
+    testData.forEach((value, index)=>{
             let ps = new shell({
             executionPolicy: 'Bypass',
             noProfile: true
@@ -124,38 +116,41 @@ $(document).ready(function(){
             ps.addCommand(`$x=$PSVersionTable;write-host '${index+1}: Swobu. ${value}'`)
             ps.invoke()
             .then(output => {
+            ps.dispose()
             doneLoading(output)
             })
             .catch(err => {
-                errorUIFeedback();
-                console.log(err);
-                ps.dispose();
+                errorUIFeedback()
+                console.log(err)
+                ps.dispose()
             });
         })
     })
 
 
     $('#test5Btn').click( () => {
-        loading()
-        const max=test4.length;
+
+        const max=testData.length;
         function rapidFirePromise(i){
             let ps = new shell({
                 executionPolicy: 'Bypass',
                 noProfile: true
                 });
                 
-                ps.addCommand(`$x=$PSVersionTable;write-host '${i+1}: idunno. ${test4[i]}'`)
+                ps.addCommand(`$x=$PSVersionTable;write-host '${i+1}: idunno. ${testData[i]}'`)
                 ps.invoke()
                 .then(output => {
+                ps.dispose()
                 doneLoading(output)
                 if (i<max-1){i++;return rapidFirePromise(i)}
                 })
+
                 .catch(err => {
-                    errorUIFeedback();
-                    console.log(err);
-                    ps.dispose();
+                    errorUIFeedback()
+                    console.log(err)
+                    ps.dispose()
                 });
              }
-        rapidFirePromise(0);
+        rapidFirePromise(0)
         })
     })
