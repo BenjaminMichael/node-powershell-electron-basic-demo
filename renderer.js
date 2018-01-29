8ac75e0552396b9e42d209ef1fc77c822cce99ec
ben <ben@m-c02sy0p7h03m.local>
2018-01-28 22:53:51 -0500

    now promoting best practice

--- renderer.js
+++ renderer.js
@@ -2,7 +2,7 @@ window.Hammer = require('./node_modules/materialize-css/js/hammer.min.js')
 require('materialize-css')
 const shell = require('node-powershell')
 
-var testData=[1,2,3,4,5,6,7,8,9]
+const testData=[1,2,3,4,5,6,7,8,9]
 
 function loading(){
     const myPreLoader = `
@@ -18,139 +18,35 @@ function loading(){
     </div>`
     $('#myH3').html(myPreLoader)
 }
-function doneLoading(myResultant){
-    $('#myH3').html('<h2>Done.</h2>')
-    const allDone = () => {
-        $('#myH3').empty()
-    }
-    if(myResultant !== ""){
-        $('#resultsGoHere').append(`<div class="chip orange lighten-3 purple-text z-depth-3 text-darken-1 col s2 m2 l2">${myResultant}</div>`)
-    }
-    setTimeout(allDone(), 100)
-}
-function errorUIFeedback(){
-    $('#myH3').html()
-    $('#resultsGoHere').append(`<div class="chip red white-text col s2 m2 l2 flow-text">Error</div>`)
-}
 
 $(document).ready(function(){
+
     $('#cancelBtn').click( () => {
-    $('#resultsGoHere').empty()
+        $('#resultsGoHere').empty()
     })
 
     $('#test1Btn').click( () => {
         loading()
+        let i=0
+        let max=testData.length
+        let runningTotal=0;
         let ps = new shell({
             executionPolicy: 'Bypass',
             noProfile: true
             });
-    testData.map((value)=>{
-        ps.streams.stdin.write(`$x=$PSVersionTable;write-host 'ZugZug: ${value}'
-        `);
-        })
-        ps.invoke()
-        ps.on('output', output=>{
-            var parsedOutput = output.split("\n") 
-            parsedOutput.forEach(val => doneLoading(val))
-            ps.dispose()
-        }); 
-    })
-
-    $('#test2Btn').click(()=>{
-       loading()
-        testData.map((value)=>{
-        setImmediate(
-            (()=>{
-            let ps = new shell({
-                executionPolicy: 'Bypass',
-                noProfile: true
-            });
-            
-            ps.addCommand(`$x=$PSVersionTable;write-host 'Work Complete: ${value}'`)
+        const doPowershell = (val) => {
+            ps.addCommand(`write-host ${val}`)
             ps.invoke()
-            .then(output => {
-                doneLoading(output)
-            })
-            .catch(err => {
-                errorUIFeedback()
-                console.log(err)
+        }
+        ps.on('output', output => {
+            runningTotal+=parseInt(output)
+            $('#resultsGoHere').append(`<div class="chip orange lighten-3 purple-text z-depth-3 text-darken-1 col s2 m2 l2">${output}</div>`)
+            if (i<max) {return doPowershell(testData[i++])}else{
                 ps.dispose()
-            });
-            })
-        )
+                $('#myH3').html('<h2>Done.</h2>')
+                $('#resultsGoHere').append(`<div class="chip orange lighten-3 purple-text z-depth-3 text-darken-1 col s2 m2 l2">total:${runningTotal}</div>`)
+            }
         })
+        doPowershell(testData[i++])
     })
-
-    $('#test3Btn').click(()=>{
-        loading()
-        testData.map((value)=>{
-            process.nextTick(() => {
-                let ps = new shell({
-                executionPolicy: 'Bypass',
-                noProfile: true
-                });
-                
-                ps.addCommand(`$x=$PSVersionTable;write-host 'Lok-Tar! ${value}'`)
-                ps.invoke()
-                .then(output => {
-                    ps.dispose();
-                    doneLoading(output)
-                })
-                .catch(err => {
-                    errorUIFeedback()
-                    console.log(err)
-                    ps.dispose()
-                });
-            });
-        })
-    })
-
-    $('#test4Btn').click( () => {
-        loading()
-    testData.forEach((value, index)=>{
-            let ps = new shell({
-            executionPolicy: 'Bypass',
-            noProfile: true
-            });
-            
-            ps.addCommand(`$x=$PSVersionTable;write-host '${index+1}: Swobu. ${value}'`)
-            ps.invoke()
-            .then(output => {
-            ps.dispose()
-            doneLoading(output)
-            })
-            .catch(err => {
-                errorUIFeedback()
-                console.log(err)
-                ps.dispose()
-            });
-        })
-    })
-
-
-    $('#test5Btn').click( () => {
-
-        const max=testData.length;
-        function rapidFirePromise(i){
-            let ps = new shell({
-                executionPolicy: 'Bypass',
-                noProfile: true
-                });
-                
-                ps.addCommand(`$x=$PSVersionTable;write-host '${i+1}: idunno. ${testData[i]}'`)
-                ps.invoke()
-                .then(output => {
-                ps.dispose()
-                doneLoading(output)
-                if (i<max-1){i++;return rapidFirePromise(i)}
-                })
-
-                .catch(err => {
-                    errorUIFeedback()
-                    console.log(err)
-                    ps.dispose()
-                });
-             }
-        rapidFirePromise(0)
-        })
     })
